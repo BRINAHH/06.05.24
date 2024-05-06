0f453f5ebcd6a07f7170d56acfa8788257ae805c
SAH <143020123+BRINAHH@users.noreply.github.com>
2024-05-06 12:25:18 -0300

    Add files via upload

---
+++ dowhile.js
@@ -0,0 +1,8 @@
+var matriz = ["vetor","vetor2","vetor3","vetor4","vetor5"];// esta determinado os vetos 
+var posicao = 0;// a posicao deles
+
+while (posicao , matriz.length) {// WHILE em ingles significa ENQUANTO, ele e uma estrutura de repeticao.
+    //vai repetir os vetores e junto sua posicao
+    console.log(matriz[posicao]);
+    posicao++;
+}
